299b677e45d041f17c9373d24918530fffbd5387
andrew <anwrew@yahoo.com>
2018-02-26 01:38:29 +0300

    [nf] security add

--- coins-web/src/main/java/ru/leon4uk/coins/app/web/initializer/AppInitializer.java
+++ coins-web/src/main/java/ru/leon4uk/coins/app/web/initializer/AppInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
