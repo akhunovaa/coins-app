1bf13125260ac20536e6075a395391b47ff4cfe2
Azat Akhunov <azat.akhunov@rtlabs.ru>
2018-02-26 21:16:38 +0300

    [nf] CurrencyPair dao init & bot thread prepare

--- coins-bot/src/main/java/ru/leon4uk/app/bot/BotApplication.java
+++ coins-bot/src/main/java/ru/leon4uk/app/bot/BotApplication.java
@@ -1,14 +1,21 @@
 package ru.leon4uk.app.bot;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
-public class BotApplication implements ApplicationListener<ContextRefreshedEvent> {
+public class BotApplication implements Runnable{
 
     private final static Logger logger = Logger.getLogger(BotApplication.class);
+    private ObjectMapper objectMapper;
 
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        logger.info("hello");
+
+    public BotApplication(int firstRialto, int secondRialto, String pair) {
+
+    }
+
+    public void run() {
+
     }
 }
---
+++ coins-web/src/main/java/ru/leon4uk/coins/app/service/CurrencyPairServiceImpl.java
@@ -0,0 +1,30 @@
+package ru.leon4uk.coins.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.leon4uk.coins.app.dao.CurrencyPairDAO;
+import ru.leon4uk.coins.app.domain.CurrencyPair;
+
+import java.util.List;
+
+@Service
+public class CurrencyPairServiceImpl implements CurrencyPairService{
+
+    @Autowired
+    private CurrencyPairDAO currencyPairDAO;
+
+    @Override
+    public void addCurrencyPair(CurrencyPair currencyPair) {
+        currencyPairDAO.addCurrencyPair(currencyPair);
+    }
+
+    @Override
+    public List<CurrencyPair> listCurrencyPair() {
+        return currencyPairDAO.listCurrencyPair();
+    }
+
+    @Override
+    public void removeCurrencyPair(Integer id) {
+        currencyPairDAO.removeCurrencyPair(id);
+    }
+}
