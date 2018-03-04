3cfc96287919137a8474ec0c62694ed85fd5eb95
Azat Akhunov <azat.akhunov@rtlabs.ru>
2018-03-04 18:54:51 +0300

    [cf] изменил работу с библиотекой

--- coins-bot/src/main/java/ru/leon4uk/app/bot/BotManager.java
+++ coins-bot/src/main/java/ru/leon4uk/app/bot/BotManager.java
@@ -1,24 +1,16 @@
 package ru.leon4uk.app.bot;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import ru.leon4uk.app.bot.impl.ComplexCollector;
-import ru.leon4uk.coins.service.Api;
-import ru.leon4uk.coins.service.RialtoEn;
+import ru.leon4uk.coins.service.ApiService;
 import ru.leon4uk.coins.service.binance.api.BinanceApi;
-import ru.leon4uk.coins.service.binance.entity.BinanceEn;
 import ru.leon4uk.coins.service.bitfinex.api.BitfinexApi;
-import ru.leon4uk.coins.service.bitfinex.entity.BitfinexEn;
 import ru.leon4uk.coins.service.bitsane.api.BitsaneApi;
-import ru.leon4uk.coins.service.bitsane.entity.BitsaneEn;
 import ru.leon4uk.coins.service.kraken.api.KrakenApi;
-import ru.leon4uk.coins.service.kraken.entity.KrakenEn;
 import ru.leon4uk.coins.service.poloniex.api.PoloniexApi;
-import ru.leon4uk.coins.service.poloniex.entity.PoloniexEn;
 import ru.leon4uk.coins.service.wex.api.WexApi;
-import ru.leon4uk.coins.service.wex.entity.WexEn;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -37,25 +29,21 @@ public class BotManager implements BotApplication{
     @Override
     public void newComplexCollectorExecuter(String firstRialto, String secondRialto, String firstCurrencyPairOne, String firstCurrencyPairTwo, String secondCurrencyPair) {
 
-        Api first;
-        Api second;
-        RialtoEn firstEn;
-        RialtoEn secondEn;
+      ApiService firtRialtoS;
+      ApiService firtRialtoHelpS;
+      ApiService secondRialtoS;
         try {
-            first = getRialto(firstRialto);
-            second = getRialto(secondRialto);
-            firstEn = getRialtoEn(firstRialto);
-            secondEn = getRialtoEn(secondRialto);
+            firtRialtoS = getRialto(firstRialto);
+            firtRialtoHelpS = getRialto(firstRialto);
+            secondRialtoS = getRialto(secondRialto);
 
             ComplexCollector complexCollector = context.getBean(ComplexCollector.class);
-            complexCollector.setFirstRialto(first);
-            complexCollector.setSecondRialto(second);
+            complexCollector.setFirstRialto(firtRialtoS);
+            complexCollector.setFirstRialtoHelp(firtRialtoHelpS);
+            complexCollector.setSecondRialto(secondRialtoS);
             complexCollector.setFirstCurrencyPairOne(firstCurrencyPairOne);
             complexCollector.setFirstCurrencyPairTwo(firstCurrencyPairTwo);
             complexCollector.setSecondCurrencyPair(secondCurrencyPair);
-            complexCollector.setFirstRialtoEn(firstEn);
-            complexCollector.setSecondRialtoEn(secondEn);
-            complexCollector.setObjectMapper(context.getBean(ObjectMapper.class));
             context.getBean(ScheduledExecutorService.class).scheduleAtFixedRate(complexCollector, 10, 10, TimeUnit.SECONDS);
 
         } catch (Exception e) {
@@ -64,7 +52,7 @@ public class BotManager implements BotApplication{
 
     }
 
-    private Api getRialto(String rialto) throws Exception {
+    private ApiService getRialto(String rialto) throws Exception {
         switch (rialto){
             case "Poloniex":
                 return new PoloniexApi("https://poloniex.com/public?command=");
@@ -78,25 +66,6 @@ public class BotManager implements BotApplication{
                 return new BitfinexApi("https://api.bitfinex.com/v1/");
             case "Bitsane":
                 return new BitsaneApi("https://bitsane.com");
-            default:
-                throw new Exception("rialto not found: " + rialto);
-        }
-    }
-
-    private RialtoEn getRialtoEn(String rialto) throws Exception {
-        switch (rialto){
-            case "Poloniex":
-                return new PoloniexEn();
-            case "Wex":
-                return new WexEn();
-            case "Binance":
-                return new BinanceEn();
-            case "Kraken":
-                return new KrakenEn();
-            case "Bitfinex":
-                return new BitfinexEn();
-            case "Bitsane":
-                return new BitsaneEn();
             default:
                 throw new Exception("rialto not found: " + rialto);
         }
