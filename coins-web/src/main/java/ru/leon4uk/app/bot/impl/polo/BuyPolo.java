5d02bb6f0bf282dd2a0abed5287acf8a53802600
Azat Akhunov <azat.akhunov@protonmail.com>
2018-03-16 02:06:47 +0300

    [bf] смена названий

--- coins-web/src/main/java/ru/leon4uk/app/bot/impl/polo/BuyPolo.java
+++ coins-web/src/main/java/ru/leon4uk/app/bot/impl/polo/BuyPolo.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import ru.leon4uk.app.bot.impl.buffer.BitsaneBuyOrderBuffer;
 import ru.leon4uk.app.bot.telegram.Telegram;
 import ru.leon4uk.coins.service.ApiService;
 import ru.leon4uk.coins.service.poloniex.entity.PoloniexOrder;
@@ -11,34 +12,35 @@ import ru.leon4uk.coins.service.poloniex.entity.PoloniexOrder;
 import java.io.IOException;
 import java.text.DecimalFormat;
 
-
 @Component
 @Scope("prototype")
-public class Sell implements Runnable {
-    private final static Logger logger = Logger.getLogger(Sell.class);
+public class BuyPolo implements Runnable {
+
+    private final static Logger logger = Logger.getLogger(BuyPolo.class);
     private ApplicationContext context;
 
     private ApiService rialto;
     private double marge;
-    private double maxBidPrice;
+    private double minAskPrice;
     private int rialtoId;
     private String pair;
 
-    public Sell() {
+
+    public BuyPolo() {
     }
 
     @Override
     public void run() {
-        logger.info("SELL: MARGE-" + marge + " PRICE-" + maxBidPrice + " PAIR-" + pair + "CURRENCY_SELL-" + "LTC");
+        logger.info("BUY: MARGE-" + marge + " PRICE-" + minAskPrice + " PAIR-" + pair + "CURRENCY_BUY-" + "LTC");
         double balance = 0.0;
-        double sellPrice = maxBidPrice;
-        double sellAmount = 0;
+        double buyPrice = minAskPrice;
+        double buyAmount = 0;
         PoloniexOrder poloniexOrder = null;
-
         try {
-            balance = Double.valueOf(rialto.getBitsaneBalance("LTC"));
-            sellAmount = balance;
-            poloniexOrder = rialto.makePoloOrder(pair, sellAmount, sellPrice, "sell");
+            balance = Double.valueOf(rialto.getBalance("USDT"));
+            buyAmount = balance / buyPrice;
+            buyAmount = Math.round(buyAmount*100000)/100000.0;
+            poloniexOrder = rialto.makePoloOrder(pair, buyAmount, buyPrice, "buy");
             logger.info("Response message \n" + poloniexOrder);
         } catch (IOException e) {
             logger.error("Ошибка получения баланса/выставлении ордера валюты", e);
@@ -50,32 +52,27 @@ public class Sell implements Runnable {
             e.printStackTrace();
         }
 
-
         if (poloniexOrder != null) {
-            logger.info("SELL ORDER" + poloniexOrder.getOrderNumber() + " RESULT: \n " + poloniexOrder.getResultingTrades());
+            logger.info("BUY ORDER" + poloniexOrder.getOrderNumber() + " RESULT: \n " + poloniexOrder.getResultingTrades());
         }
 
-            logger.info("SOLD: MARGE-" + marge + " PRICE-" + sellPrice + " PAIR-" + pair + "CURRENCY_SELL-" + "USDT");
+            logger.info("BOUGHT: MARGE-" + marge + " PRICE-" + minAskPrice + " PAIR-" + pair + "CURRENCY_BUY-" + "LTC");
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("<b>[CREATE ORDER SELL]</b>").append("\n");
-            stringBuilder.append("<b>QTY: </b>").append(sellAmount).append(" [").append(pair).append("] ").append(" (").append(sellPrice).append(") ").append(" [").append(maxBidPrice).append("] ").append("\n");
+            stringBuilder.append("<b>[CREATE ORDER BUY]</b>").append("\n");
+            stringBuilder.append("<b>QTY: </b>").append(buyAmount).append(" [").append(pair).append("] ").append(" (").append(buyPrice).append(") ").append(" [").append(minAskPrice).append("] ").append("\n");
             stringBuilder.append("<b>Diff: </b>").append(new DecimalFormat("#.#####").format(marge)).append("\n");
-            if (poloniexOrder != null) {
+        if (poloniexOrder != null) {
             stringBuilder.append("<b>ID: </b>").append(poloniexOrder.getOrderNumber()).append("\n");
             stringBuilder.append("<b>RESULT: </b>").append("\n").append(poloniexOrder.getResultingTrades()).append("\n");
         }
-            context.getBean(Telegram.class).sendMessage(stringBuilder.toString());
+
+        context.getBean(Telegram.class).sendMessage(stringBuilder.toString());
+           context.getBean(BitsaneBuyOrderBuffer.class).setStatus(Boolean.FALSE);
 
         synchronized (this) {
             this.notify();
-            logger.info("Thread Sell notify!");
+            logger.info("Thread Buy notify!");
         }
-    }
-
-    private double sell(double sellPrice, double sellAmount, double sellFee) {
-        double buy = sellAmount * sellPrice;
-        double fee = sellAmount / 100 * sellFee;
-        return buy - fee;
     }
 
 
@@ -87,12 +84,12 @@ public class Sell implements Runnable {
         this.marge = marge;
     }
 
-    public double getMaxBidPriceTwo() {
-        return maxBidPrice;
+    public double getMinAskPrice() {
+        return minAskPrice;
     }
 
-    public void setMaxBidPriceTwo(double maxBidPriceTwo) {
-        this.maxBidPrice = maxBidPriceTwo;
+    public void setMinAskPrice(double minAskPriceTwo) {
+        this.minAskPrice = minAskPriceTwo;
     }
 
     public int getRialtoId() {
--- coins-web/src/main/java/ru/leon4uk/app/bot/impl/polo/SellPolo.java
+++ coins-web/src/main/java/ru/leon4uk/app/bot/impl/polo/SellPolo.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import ru.leon4uk.app.bot.impl.buffer.BitsaneBuyOrderBuffer;
 import ru.leon4uk.app.bot.telegram.Telegram;
 import ru.leon4uk.coins.service.ApiService;
 import ru.leon4uk.coins.service.poloniex.entity.PoloniexOrder;
@@ -12,35 +11,34 @@ import ru.leon4uk.coins.service.poloniex.entity.PoloniexOrder;
 import java.io.IOException;
 import java.text.DecimalFormat;
 
+
 @Component
 @Scope("prototype")
-public class Buy implements Runnable {
-
-    private final static Logger logger = Logger.getLogger(Buy.class);
+public class SellPolo implements Runnable {
+    private final static Logger logger = Logger.getLogger(SellPolo.class);
     private ApplicationContext context;
 
     private ApiService rialto;
     private double marge;
-    private double minAskPrice;
+    private double maxBidPrice;
     private int rialtoId;
     private String pair;
 
-
-    public Buy() {
+    public SellPolo() {
     }
 
     @Override
     public void run() {
-        logger.info("BUY: MARGE-" + marge + " PRICE-" + minAskPrice + " PAIR-" + pair + "CURRENCY_BUY-" + "LTC");
+        logger.info("SELL: MARGE-" + marge + " PRICE-" + maxBidPrice + " PAIR-" + pair + "CURRENCY_SELL-" + "LTC");
         double balance = 0.0;
-        double buyPrice = minAskPrice;
-        double buyAmount = 0;
+        double sellPrice = maxBidPrice;
+        double sellAmount = 0;
         PoloniexOrder poloniexOrder = null;
+
         try {
-            balance = Double.valueOf(rialto.getBalance("USDT"));
-            buyAmount = balance / buyPrice;
-            buyAmount = Math.round(buyAmount*100000)/100000.0;
-            poloniexOrder = rialto.makePoloOrder(pair, buyAmount, buyPrice, "buy");
+            balance = Double.valueOf(rialto.getBitsaneBalance("LTC"));
+            sellAmount = balance;
+            poloniexOrder = rialto.makePoloOrder(pair, sellAmount, sellPrice, "sell");
             logger.info("Response message \n" + poloniexOrder);
         } catch (IOException e) {
             logger.error("Ошибка получения баланса/выставлении ордера валюты", e);
@@ -52,27 +50,32 @@ public class Buy implements Runnable {
             e.printStackTrace();
         }
 
+
         if (poloniexOrder != null) {
-            logger.info("BUY ORDER" + poloniexOrder.getOrderNumber() + " RESULT: \n " + poloniexOrder.getResultingTrades());
+            logger.info("SELL ORDER" + poloniexOrder.getOrderNumber() + " RESULT: \n " + poloniexOrder.getResultingTrades());
         }
 
-            logger.info("BOUGHT: MARGE-" + marge + " PRICE-" + minAskPrice + " PAIR-" + pair + "CURRENCY_BUY-" + "LTC");
+            logger.info("SOLD: MARGE-" + marge + " PRICE-" + sellPrice + " PAIR-" + pair + "CURRENCY_SELL-" + "USDT");
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("<b>[CREATE ORDER BUY]</b>").append("\n");
-            stringBuilder.append("<b>QTY: </b>").append(buyAmount).append(" [").append(pair).append("] ").append(" (").append(buyPrice).append(") ").append(" [").append(minAskPrice).append("] ").append("\n");
+            stringBuilder.append("<b>[CREATE ORDER SELL]</b>").append("\n");
+            stringBuilder.append("<b>QTY: </b>").append(sellAmount).append(" [").append(pair).append("] ").append(" (").append(sellPrice).append(") ").append(" [").append(maxBidPrice).append("] ").append("\n");
             stringBuilder.append("<b>Diff: </b>").append(new DecimalFormat("#.#####").format(marge)).append("\n");
-        if (poloniexOrder != null) {
+            if (poloniexOrder != null) {
             stringBuilder.append("<b>ID: </b>").append(poloniexOrder.getOrderNumber()).append("\n");
             stringBuilder.append("<b>RESULT: </b>").append("\n").append(poloniexOrder.getResultingTrades()).append("\n");
         }
-
-        context.getBean(Telegram.class).sendMessage(stringBuilder.toString());
-           context.getBean(BitsaneBuyOrderBuffer.class).setStatus(Boolean.FALSE);
+            context.getBean(Telegram.class).sendMessage(stringBuilder.toString());
 
         synchronized (this) {
             this.notify();
-            logger.info("Thread Buy notify!");
+            logger.info("Thread Sell notify!");
         }
+    }
+
+    private double sell(double sellPrice, double sellAmount, double sellFee) {
+        double buy = sellAmount * sellPrice;
+        double fee = sellAmount / 100 * sellFee;
+        return buy - fee;
     }
 
 
@@ -84,12 +87,12 @@ public class Buy implements Runnable {
         this.marge = marge;
     }
 
-    public double getMinAskPrice() {
-        return minAskPrice;
+    public double getMaxBidPriceTwo() {
+        return maxBidPrice;
     }
 
-    public void setMinAskPrice(double minAskPriceTwo) {
-        this.minAskPrice = minAskPriceTwo;
+    public void setMaxBidPriceTwo(double maxBidPriceTwo) {
+        this.maxBidPrice = maxBidPriceTwo;
     }
 
     public int getRialtoId() {
